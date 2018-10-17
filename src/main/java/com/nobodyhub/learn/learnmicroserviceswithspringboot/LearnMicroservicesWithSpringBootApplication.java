dc0fe44f5666440d5335ac831ee7a7b23f037a1f
yan_h <yan_h@hotmail.com>
2018-10-17 22:32:50 +0800

    init project

---
+++ src/main/java/com/nobodyhub/learn/learnmicroserviceswithspringboot/LearnMicroservicesWithSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.nobodyhub.learn.learnmicroserviceswithspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnMicroservicesWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnMicroservicesWithSpringBootApplication.class, args);
+	}
+}
---
+++ src/test/java/com/nobodyhub/learn/learnmicroserviceswithspringboot/LearnMicroservicesWithSpringBootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nobodyhub.learn.learnmicroserviceswithspringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LearnMicroservicesWithSpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
