c9914cad67bdeda59a69d7ef9bacefc3f59daf0e
yan_h <yan_h@hotmail.com>
2018-11-08 16:39:55 +0800

    add domain classes for the gamification

---
+++ gamification/src/main/java/com/nobodyhub/learn/gamification/domain/Badge.java
@@ -0,0 +1,17 @@
+package com.nobodyhub.learn.gamification.domain;
+
+/**
+ * Enumeration with the different types of Badges that a user can win.
+ *
+ * @author yan_h
+ */
+public enum Badge {
+    // Badges depending on score
+    BRONZE_MULTIPLICATOR,
+    SILVER_MULTIPLICATOR,
+    GOLD_MULTIPLICATOR,
+
+    // Other badges won for different conditions
+    FIRST_ATTEMPT,
+    FIRST_WON
+}
