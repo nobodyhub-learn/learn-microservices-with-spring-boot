1b8fdc6d6f3fab540b4650e3e6288f03f2a10e42
yan_h <yan_h@hotmail.com>
2018-11-08 15:30:20 +0800

    move multiplication code to separate folder

--- multiplication/src/main/java/com/nobodyhub/learn/multiplication/LearnMicroservicesWithSpringBootApplication.java
+++ multiplication/src/main/java/com/nobodyhub/learn/multiplication/LearnMicroservicesWithSpringBootApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LearnMicroservicesWithSpringBootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LearnMicroservicesWithSpringBootApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LearnMicroservicesWithSpringBootApplication.class, args);
+    }
 }
